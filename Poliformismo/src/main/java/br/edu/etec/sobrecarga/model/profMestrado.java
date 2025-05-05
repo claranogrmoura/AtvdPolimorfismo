cf063be40e6fa5439e0dc925b0f348c1abd03d7b
Clara Nogueira <claranogueirareis75@gmail.com>
2025-05-05 17:05:02 -0300

    atvd polimorfismo

--- Poliformismo/src/main/java/br/edu/etec/sobrecarga/model/profMestrado.java
+++ Poliformismo/src/main/java/br/edu/etec/sobrecarga/model/profMestrado.java
@@ -9,5 +9,27 @@ package br.edu.etec.sobrecarga.model;
  * @author CLARA
  */
 public class profMestrado {
-    
+    private int artigos;
+
+    public ProfessorMestrado(String nome, int disciplinas, int artigos) {
+        super(nome, disciplinas);
+        this.artigos = artigos;
+    }
+
+   
+    public double Salario(double salarioBruto, int disciplinas, int artigos) {
+        double salarioBase = super.Salario(salarioBruto, disciplinas);
+        double adicionalArtigos = artigos * 150;
+        return salarioBase + adicionalArtigos;
+    }
+
+    @Override
+    public void info() {
+        double salarioLiquido = Salario(salarioBruto, disciplinas, artigos);
+        System.out.println("Professor de Mestrado: " + nome);
+        System.out.println("Disciplinas: " + disciplinas);
+        System.out.println("Artigos Publicados: " + artigos);
+        System.out.printf("Salário Líquido: R$ %.2f%n", salarioLiquido);
+    }
+}
 }
